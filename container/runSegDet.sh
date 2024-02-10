6cfbf0fc7d0b7393786a63d496f292933ccf9fdd
Timotej979 <timotej.pet@gmail.com>
2024-02-11 00:32:35 +0100

    Add new DS and run script

--- container/runSegDet.sh
+++ container/runSegDet.sh
@@ -4,8 +4,9 @@
 printenv | grep WAND
 
 # Run segmentation
-python3 /app/container/segmentation/run.py --prepare --resize-prepaired --resize-prepaired-size 512,384
+#python3 /app/container/segmentation/run.py --prepare --resize-prepaired --resize-prepaired-size 512,384
 #python3 /app/container/segmentation/run.py --prepare --resize-prepaired --resize-prepaired-size 512,384 --train --test
 
 # Run detection
-python3 /app/container/detection/run.py --prepare --resize-prepaired --resize-prepaired-size 512,384
+python3 /app/container/detection/run.py --prepare --resize-prepaired --resize-prepaired-size 512,384 --autolabel --autolabel-method contours
+python3 /app/container/detection/run.py --prepare --resize-prepaired --resize-prepaired-size 512,384 --autolabel --autolabel-method autodistil 
