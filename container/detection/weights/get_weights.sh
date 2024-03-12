f83cf76bd6c2362b951b258768cff1f6154fdf3d
Timotej979 <timotej.pet@gmail.com>
2024-03-12 21:14:34 +0100

    Add automatic weights download, update .gitignore

---
+++ container/detection/weights/get_weights.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Pull YOLOv8 weights to directory
+wget https://github.com/ultralytics/assets/releases/download/v8.1.0/yolov8n.pt
+wget https://github.com/ultralytics/assets/releases/download/v8.1.0/yolov8s.pt
+wget https://github.com/ultralytics/assets/releases/download/v8.1.0/yolov8m.pt
+wget https://github.com/ultralytics/assets/releases/download/v8.1.0/yolov8l.pt
+wget https://github.com/ultralytics/assets/releases/download/v8.1.0/yolov8x.pt
--- run.sh
+++ run.sh
@@ -3,6 +3,26 @@
 # Run the environment initialization script
 source .env
 
+# Check if the current_dir + /container/detection/weights directory exists
+WEIGHTS_DIR="$PWD/container/detection/weights"
+
+if [ ! -d "$WEIGHTS_DIR" ]; then
+    # Create the /container/detection/weights directory
+    echo "Creating the /container/detection/weights directory"
+    mkdir -p $WEIGHTS_DIR
+fi
+
+# Check if the directory has yolov8n/s/m/l/x.pt files
+if [ ! -f "$WEIGHTS_DIR/yolov8n.pt" ] || [ ! -f "$WEIGHTS_DIR/yolov8s.pt" ] || [ ! -f "$WEIGHTS_DIR/yolov8m.pt" ] || [ ! -f "$WEIGHTS_DIR/yolov8l.pt" ] || [ ! -f "$WEIGHTS_DIR/yolov8x.pt" ]; then
+    # Redownload the weights files
+    echo "Redownloading the weights files"
+    wget -O $WEIGHTS_DIR/yolov8n.pt https://github.com/ultralytics/assets/releases/download/v8.1.0/yolov8n.pt
+    wget -O $WEIGHTS_DIR/yolov8s.pt https://github.com/ultralytics/assets/releases/download/v8.1.0/yolov8s.pt
+    wget -O $WEIGHTS_DIR/yolov8m.pt https://github.com/ultralytics/assets/releases/download/v8.1.0/yolov8m.pt
+    wget -O $WEIGHTS_DIR/yolov8l.pt https://github.com/ultralytics/assets/releases/download/v8.1.0/yolov8l.pt
+    wget -O $WEIGHTS_DIR/yolov8x.pt https://github.com/ultralytics/assets/releases/download/v8.1.0/yolov8x.pt
+fi
+
 
 # Check command arguments for ./run.sh
 if [ "$1" == "cpu" ]; then
